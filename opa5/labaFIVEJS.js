a1357e00ee4ada06e0718e7b47a048843d9861e0
VanillaChoc <90957909+VanillaChoc@users.noreply.github.com>
2021-11-03 20:02:04 +0700

    lab5

---
+++ opa5/labaFIVEJS.js
@@ -0,0 +1,24 @@
+open1.onclick= function(){
+    backk.style.display = "block";
+    o1.style.display = "block";
+}
+open2.onclick= function(){
+    backk.style.display = "block";
+    o3.style.display = "block";
+}
+open3.onclick= function(){
+    backk.style.display = "block";
+    o2.style.display = "block";
+}
+o2.onclick= function(){
+    backk.style.display = "none";
+    o2.style.display = "none";
+}
+o3.onclick= function(){
+    backk.style.display = "none";
+    o3.style.display = "none";
+}
+o1.onclick= function(){
+    backk.style.display = "none";
+    o1.style.display = "none";
+}
